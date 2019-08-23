fcfcf345f654d4b1d1550d647d478a927b684411
markopol0 <ezzati.saeid@gmail.com>
2019-08-23 11:33:55 -0700

    add animation

--- src/Dogs.js
+++ src/Dogs.js
@@ -1,6 +1,8 @@
 
 import React from 'react';
 import './Dogs.css';
+import Lottie from 'lottie-react-web'
+import animation from './dog_wagging.json'
 
 class Dogs extends React.Component {
   constructor(props) {
@@ -13,7 +15,7 @@ class Dogs extends React.Component {
   componentDidMount() {
     fetch('https://api.sheety.co/1dd8774f-b626-4446-8c7d-86f5cd5624ee')
       .then(response => response.json())
-      .then(data => this.setState({ dogList: data }));
+      .then(data => setTimeout(()=>this.setState({ dogList: data }), 2000) );
   }
 
 
@@ -25,25 +27,37 @@ class Dogs extends React.Component {
       <div className="Dog">
         <header className="Dog-header">
           <h1>{dogList.length} Dogs</h1>
-          <div className = "Dog-buttons">
-            <div className = "button">Sign up to walk</div>
+          <div className="Dog-buttons">
+            <div className="button">Sign up to walk</div>
           </div>
         </header>
         <div className="Dog-details">
-          {dogList.map(dog => {
-            return (
-              <div className="Dog-box" onClick={handleClickSection("dog-detail", dog)}>
-                <img className="Dog-img" alt="doggies!" src={dog.profilePhoto}></img>
-                <div className="Dog-info">
-                  <div className ="Dog-info-header">
-                    <span>{dog.dogName}</span>
-                    <span>{dog.officeLocation}</span>
-                  </div>
-                  <div className ="Dog-owner">{dog.owner}</div>
-                </div>
+          {dogList.length === 0
+            ? <div className="Dog-animation">
+              <Lottie
+              ariaRole=''
+              options={{
+                animationData: animation,
+                autoplay: true,
+                loop: true,
+              }}
+              />
               </div>
-            )
-          })}
+            : dogList.map(dog => {
+              return (
+                <div className="Dog-box" onClick={handleClickSection("dog-detail", dog)}>
+                  <img className="Dog-img" alt="doggies!" src={dog.profilePhoto}></img>
+                  <div className="Dog-info">
+                    <div className="Dog-info-header">
+                      <span>{dog.dogName}</span>
+                      <span>{dog.officeLocation}</span>
+                    </div>
+                    <div className="Dog-owner">{dog.owner}</div>
+                  </div>
+                </div>
+              )
+            })
+          }
         </div>
       </div>
     );
